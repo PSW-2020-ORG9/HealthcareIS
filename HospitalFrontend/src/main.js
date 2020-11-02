b1665c46ac107fe809d0299c6c5d8da13d5e44e5
sStbacki <strbacki.ra166.2017@uns.ac.rs>
2020-11-02 20:20:41 +0100

    Create form on frontend

--- HospitalFrontend/src/main.js
+++ HospitalFrontend/src/main.js
@@ -6,4 +6,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
 createApp(App).use(router).mount('#app')
+
--- HospitalFrontend/src/views/CreateFeedback.vue
+++ HospitalFrontend/src/views/CreateFeedback.vue
@@ -1,13 +1,43 @@
 <template>
-  <h1>This is a CreateFeedback page for a patient</h1>
+
+  <div class="container">
+    <div class="col-sm-3 mx-auto shadow-lg p-3 mb-5 bg-white rounded feedback">
+      <div class="col-sm d-flex justify-content-start">
+        <div class="form-group d-flex justify-content-start">
+          <textarea class="form-control" rows=5 type="text" placeholder="Enter your feedback"></textarea>
+        </div>
+      </div>
+      <div class="col-sm offset-md-1 d-flex justify-content-start">
+        <div class="form-group">
+          <input type="checkbox" class="form-check-input" id="public-feedback">
+          <label class="form-check-label" for="public-feedback">Public</label>
+        </div>
+      </div>
+      <div class="col-sm offset-md-1 d-flex justify-content-start">
+        <div class="form-group">
+          <input type="checkbox" class="form-check-input" id="anon-feedback">
+          <label class="form-check-label" for="anon-feedback">Send anonimously</label>
+        </div>
+      </div>
+      <button type="submit" class="btn submit-btn">Submit</button>
+    </div>
+  </div>
+
 </template>
 
 <script>
-export default {
-  name:'CreateFeedback'
-}
+  export default {
+    name: 'CreateFeedback',
+    methods: {
+
+    }
+  }
 </script>
 
 <style>
+.col-md-1-5 { width: 5,5%; }
+.submit-btn {
+  background-color: #58804f;
+}
 
 </style>
