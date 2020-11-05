83d6340824514a862f9088a03b31a1c9279ca5c7
dracdev <darjan21+1@hotmail.com>
2020-11-05 15:03:16 +0100

    Initialised Feature View

---
+++ HospitalFrontend/src/components/FeedbackItem.vue
@@ -0,0 +1,35 @@
+<template>
+
+
+    <div class="card">
+        <h5 class="card-header">{{feedback.UserAccount.Username}}}</h5>
+        <div class="card-body">           
+            <p class="card-text">{{feedback.UserAccount}}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name:'ObserveFeedback',
+  props:{
+      feedback: Object
+  },
+
+  
+  methods: {},
+  mounted: function(){
+    
+      
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
--- HospitalFrontend/src/views/ObserveFeedback.vue
+++ HospitalFrontend/src/views/ObserveFeedback.vue
@@ -1,10 +1,32 @@
 <template>
   <h1>This is a ObserveFeedback page for any user and admin </h1>
+  <div id="feedbackList" class="list-group">
+    <FeedbackItem v-for="feedback in feedbacks" v-bind:key="feedback.Id" feedback ="feedback"></FeedbackItem>
+
+
+  </div>
 </template>
 
 <script>
+import FeedbackItem from './components/FeedbackItem.vue'
+
 export default {
-  name:'ObserveFeedback'
+  name:'ObserveFeedback',
+  data: function(){
+      return{
+        feedbacks:[]
+
+      }
+  },
+  components:{
+    FeedbackItem
+  },
+  methods: {},
+  mounted: function(){
+    
+      
+    
+  }
 }
 </script>
 
