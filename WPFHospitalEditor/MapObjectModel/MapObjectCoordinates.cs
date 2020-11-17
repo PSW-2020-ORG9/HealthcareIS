3b8ebf8fd5f7701a378d2b7016e54674c17c90f4
denisfruza <denisfruza98@hotmail.com>
2020-11-17 16:57:54 +0100

    mapObject metrics and rectangle position

---
+++ WPFHospitalEditor/MapObjectModel/MapObjectCoordinates.cs
@@ -0,0 +1,14 @@
+﻿namespace WPFHospitalEditor.MapObjectModel
+{
+    public class MapObjectCoordinates
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public MapObjectCoordinates(double X, double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+}
