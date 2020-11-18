4e028dbf28742134323d33951cc1f3204f9c8cfe
Dimitrije Bulaja <dbulaja98@gmail.com>
2020-11-18 13:50:46 +0100

    login and hospitalMap windows created

--- WPFHospitalEditor/HospitalMap.xaml.cs
+++ WPFHospitalEditor/HospitalMap.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,25 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WPFHospitalEditor
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for HospitalMap.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class HospitalMap : Window
     {
-        public MainWindow()
+        public HospitalMap()
         {
             InitializeComponent();
-        }
-        private void hospitalMapPressed(object sender, RoutedEventArgs e)
-        {
-            HospitalMap startWindow = new HospitalMap();
-            this.Close();
-            startWindow.ShowDialog();
         }
     }
 }
