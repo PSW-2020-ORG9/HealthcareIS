603554215a3b2555f6731841edfbea9308b2cb49
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2020-11-20 17:15:51 +0100

    Created patient tests

--- HealthcareBaseTests/MedicalRecordTests.cs
+++ HealthcareBaseTests/MedicalRecordTests.cs
@@ -1,13 +1,69 @@
+using Model.Users.Patient;
+using Moq;
+using Repository.ScheduleRepository.HospitalizationsRepository;
+using Repository.ScheduleRepository.ProceduresRepository;
+using Repository.UsersRepository.EmployeesAndPatientsRepository;
+using Service.UsersService.PatientService;
+using System.Collections.Generic;
 using Xunit;
 
 namespace HealthcareBaseTests
 {
     public class MedicalRecordTests
     {
-        [Fact]
-        public void Finds_patient_medical_record()
+        private Mock<PatientRepository> _patientStubRepository;
+        private Mock<ExaminationRepository> _examinationStubRepository;
+        private Mock<SurgeryRepository> _surgeryStubRepository;
+        private Mock<HospitalizationRepository> _hospitalizationStubRepository;
+
+        private void PrepareStubs()
         {
-            
+            _patientStubRepository = new Mock<PatientRepository>();
+            _examinationStubRepository = new Mock<ExaminationRepository>();
+            _surgeryStubRepository = new Mock<SurgeryRepository>();
+            _hospitalizationStubRepository = new Mock<HospitalizationRepository>();
+
+            Patient p = new Patient();
+            p.MedicalRecordID = 1;
+            p.Name = "Pera";
+            p.Surname = "Peric";
+
+            List<Patient> patients = new List<Patient>();
+            patients.Add(p);
+
+            _patientStubRepository.Setup(m => m.GetByID(1)).Returns(p);
+            _patientStubRepository.Setup(m => m.GetAll()).Returns(patients);
+        }
+
+        [Fact]
+        public void Finds_patient()
+        {
+            PrepareStubs();
+            PatientService service = new PatientService
+                (
+                _patientStubRepository.Object,
+                _examinationStubRepository.Object,
+                _surgeryStubRepository.Object,
+                _hospitalizationStubRepository.Object
+                );
+
+            Patient result = service.GetByID(1);
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void Finds_no_patient()
+        {
+            PrepareStubs();
+            PatientService service = new PatientService
+                (
+                _patientStubRepository.Object,
+                _examinationStubRepository.Object,
+                _surgeryStubRepository.Object,
+                _hospitalizationStubRepository.Object
+                );
+            Patient result = service.GetByID(2);
+            Assert.Null(result);
         }
     }
 }
