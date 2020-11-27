255d17a46071b91e5b50389ba6d01c6f58d6f6ad
denisfruza <denisfruza98@hotmail.com>
2020-11-27 14:37:10 +0100

    equipment dto and adapter

---
+++ HealthcareBase/Model/EditorAdapters/EquipmentAdapter.cs
@@ -0,0 +1,20 @@
+﻿using HealthcareBase.Model.EditorDtos;
+using HealthcareBase.Model.HospitalResources;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthcareBase.Model.EditorAdapters
+{
+    public class EquipmentAdapter
+    {
+        public static EquipmentUnit DtoToObject(EquipmentDto dto)
+        {
+            return new EquipmentUnit
+            {
+
+
+            };
+        }
+    }
+}
---
+++ HealthcareBase/Model/EditorDtos/EquipmentDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HealthcareBase.Model.EditorDtos
+{
+    public class EquipmentDto
+    {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+        public String Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
