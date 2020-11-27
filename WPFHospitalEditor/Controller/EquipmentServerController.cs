52ee6c0fc3b9b99fe6c97e16575c58c69e9387b3
denisfruza <denisfruza98@hotmail.com>
2020-11-27 14:11:44 +0100

    created http request for getting equipment by room id

---
+++ WPFHospitalEditor/Controller/EquipmentServerController.cs
@@ -0,0 +1,18 @@
+﻿using HealthcareBase.Model.HospitalResources;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WPFHospitalEditor.Service;
+
+namespace WPFHospitalEditor.Controller
+{
+   public class EquipmentServerController
+    {
+        public EquipmentServerService EquipmentServerService = new EquipmentServerService();
+
+        public IEnumerable<EquipmentUnit> getEquipmentByRoomId(int roomId)
+        {
+            return EquipmentServerService.getEquipmentByRoomId(roomId);
+        }
+    }
+}
---
+++ WPFHospitalEditor/Service/EquipmentServerService.cs
@@ -0,0 +1,20 @@
+﻿using HealthcareBase.Model.HospitalResources;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFHospitalEditor.Service
+{
+   public  class EquipmentServerService
+    {
+        public IEnumerable<EquipmentUnit> getEquipmentByRoomId(int roomId)
+        {
+            var client = new RestSharp.RestClient("http://localhost:****/");
+            var request = new RestRequest("api/Equipment/getByRoomId" + roomId, Method.GET);
+            var response = client.Get<List<EquipmentUnit>>(request);
+            IEnumerable<EquipmentUnit> result = response.Data;
+            return result;
+        }
+    }
+}
