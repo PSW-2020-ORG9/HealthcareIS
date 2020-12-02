d1bf601401c11a54030f8510d31b730ddf99723f
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2020-12-03 00:30:14 +0100

    Added integration tests

---
+++ HospitalWebAppTests/Context/MySqlTestContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using HealthcareBase.Model.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalWebAppIntegrationTests.Context
+{
+    internal class MySqlTestContextFactory : IContextFactory
+    {
+        private readonly string _connectionString;
+        public MySqlTestContextFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public DbContext CreateContext()
+        {
+            DbContext context = new MySqlTestContext(_connectionString);
+            context.Database.EnsureCreated();
+            return context;
+        }
+    }
+}
