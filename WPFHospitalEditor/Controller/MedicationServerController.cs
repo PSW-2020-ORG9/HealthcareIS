22a1c52c1c163805ea3bd5f03da1540455fdd2de
Dimitrije Bulaja <dbulaja98@gmail.com>
2020-12-02 20:57:02 +0100

    Added MedicationServerService and MedicationServerController with methods for getting all medication

---
+++ WPFHospitalEditor/Controller/MedicationServerController.cs
@@ -0,0 +1,16 @@
+﻿using HealthcareBase.Dto;
+using System.Collections.Generic;
+using WPFHospitalEditor.Service;
+
+namespace WPFHospitalEditor.Controller
+{
+    public class MedicationServerController
+    {
+        public MedicationServerService medicationServerService = new MedicationServerService();
+
+        public IEnumerable<MedicationDto> getAllMedication()
+        {
+            return medicationServerService.getAllMedication();
+        }
+    }
+}
---
+++ WPFHospitalEditor/Service/MedicationServerService.cs
@@ -0,0 +1,17 @@
+﻿using HealthcareBase.Dto;
+using RestSharp;
+using System.Collections.Generic;
+
+namespace WPFHospitalEditor.Service
+{
+   public  class MedicationServerService
+    {
+        public IEnumerable<MedicationDto> getAllMedication()
+        {
+            var client = new RestClient(AllConstants.connectionUrl);
+            var request = new RestRequest("Medication/getAll/", Method.GET);
+            var response = client.Get<IEnumerable<MedicationDto>>(request);
+            return response.Data;
+        }
+    }
+}
