255482b9e59c1efc8e13eafcc1dfb360e38edd7b
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2020-12-03 20:24:06 +0100

    Revert "Refactor: deleted interfaces which were not used"
    This reverts commit d4159b50400112a7cd22eaada54f99ef7e1f56dd.

--- HealthcareBase/Repository/HospitalResourcesRepository/EquipmentSqlRepository.cs
+++ HealthcareBase/Repository/HospitalResourcesRepository/EquipmentSqlRepository.cs
@@ -2,19 +2,27 @@
 using HealthcareBase.Model.HospitalResources;
 using HealthcareBase.Repository.Generics;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace HealthcareBase.Repository.HospitalResourcesRepository
 {
-    public class EquipmentSqlRepository : GenericSqlRepository<EquipmentUnit, int>
+    public class EquipmentSqlRepository : GenericSqlRepository<EquipmentUnit, int>, IEquipmentUnitRepository
     {
         public EquipmentSqlRepository(IContextFactory contextFactory) : base(contextFactory) { }
+
+        public IEnumerable<EquipmentUnit> GetByCurrentLocationWithoutParse(Room room)
+        {
+            throw new NotImplementedException();
+        }
 
         protected override IQueryable<EquipmentUnit> IncludeFields(IQueryable<EquipmentUnit> query)
         {
             return query.
                 Include(unit => unit.CurrentLocation)
                 .ThenInclude(location => location.Department)
+
                 .Include(unit => unit.EquipmentType);
         }
     }
--- HealthcareBase/Repository/MedicationRepository/MedicationSqlRepository.cs
+++ HealthcareBase/Repository/MedicationRepository/MedicationSqlRepository.cs
@@ -1,13 +1,35 @@
 ﻿using HealthcareBase.Model.Database;
 using HealthcareBase.Model.Medication;
 using HealthcareBase.Repository.Generics;
+using HealthcareBase.Repository.MedicationRepository.Interface;
+using System;
+using System.Linq;
 
 namespace HealthcareBase.Repository.MedicationRepository
 {
-    public class MedicationSqlRepository : GenericSqlRepository<Medication, int>
+    public class MedicationSqlRepository : GenericSqlRepository<Medication, int>, IMedicationRepository
     {
         public MedicationSqlRepository(IContextFactory contextFactory) : base(contextFactory)
         {
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        protected override IQueryable<Medication> IncludeFields(IQueryable<Medication> query)
+        {
+            throw new NotImplementedException();
         }
     }
 }
