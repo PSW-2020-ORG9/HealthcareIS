ae926734e40dc7a67e2406ea040a1cd921d52c0a
Dimitrije Bulaja <dbulaja98@gmail.com>
2020-12-04 19:48:53 +0100

    Implemented methods for getting all equipmentTypes in WPFHospitalEditor

---
+++ WPFHospitalEditor/Controller/EquipmentTypeServerController.cs
@@ -0,0 +1,17 @@
+﻿using HealthcareBase.Dto;
+using System.Collections.Generic;
+using WPFHospitalEditor.Service;
+using WPFHospitalEditor.Service.Interface;
+
+namespace WPFHospitalEditor.Controller
+{
+    public class EquipmentTypeServerController
+    {
+        private IEquipmentTypeServerService EquipmentTypeServerService = new EquipmentTypeServerService();
+
+        public IEnumerable<EquipmentTypeDto> GetAllEquipmentTypes()
+        {
+            return EquipmentTypeServerService.GetAllEquipmentTypes();
+        }
+    }
+}
