7765d548b87d3dd9f14ebe46052f98ac901d268d
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2020-12-14 13:11:55 +0100

    Fix: Restored empty constructor

--- HospitalWebAppIntegrationTests/Context/MySqlTestContext.cs
+++ HospitalWebAppIntegrationTests/Context/MySqlTestContext.cs
@@ -15,6 +15,7 @@ namespace HospitalWebAppIntegrationTests.Context
 {
     internal class MySqlTestContext : MySqlContext
     {
+        public MySqlTestContext() { }
         public MySqlTestContext(string connectionString) : base(connectionString) { }
         protected override void SeedData(ModelBuilder modelBuilder)
         {
