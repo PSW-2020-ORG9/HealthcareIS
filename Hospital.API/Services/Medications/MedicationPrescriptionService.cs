5645dcf6175be90511d895cda6d0ff320249be61
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2021-01-04 14:34:30 +0100

    Refactor: Attach diagnoses to prescriptions

--- Hospital.API/Services/Medications/MedicationPrescriptionService.cs
+++ Hospital.API/Services/Medications/MedicationPrescriptionService.cs
@@ -31,15 +31,18 @@ namespace Hospital.API.Services.Medications
         }
 
         public IEnumerable<MedicationPrescription> SimpleSearch(string nameQuery)
-            => _medicationPrescriptionWrapper.Repository.GetMatching(
+        {
+            var prescriptions = _medicationPrescriptionWrapper.Repository.GetMatching(
                 prescription => prescription.Medication.Name.Contains(nameQuery));
+            return AttachDiagnoses(prescriptions);
+        }
 
         public IEnumerable<MedicationPrescription> AdvancedSearch(PrescriptionAdvancedFilterDto filterDto)
         {
             var prescriptions = _medicationPrescriptionWrapper.Repository.GetMatching(filterDto.GetFilterExpression());
             if (!string.IsNullOrEmpty(filterDto.Diagnosis))
                 prescriptions = FilterByDiagnosis(prescriptions, filterDto.Diagnosis);
-            return prescriptions;
+            return AttachDiagnoses(prescriptions);
         }
 
         private IEnumerable<MedicationPrescription> FilterByDiagnosis(IEnumerable<MedicationPrescription> prescriptions, string diagnosisName)
@@ -51,10 +54,25 @@ namespace Hospital.API.Services.Medications
             return prescriptions.Where(p => diagnoses.Contains(p.DiagnosisId));
         }
 
+        private IEnumerable<MedicationPrescription> AttachDiagnoses(IEnumerable<MedicationPrescription> prescriptions)
+        {
+            List<int> diagnosisIds = new List<int>();
+            foreach (var prescription in prescriptions)
+            {
+                diagnosisIds.Add(prescription.DiagnosisId);
+            }
+            IEnumerable<Diagnosis> diagnoses = FetchDiagnoses(diagnosisIds);
+            foreach (var prescription in prescriptions)
+            {
+                prescription.Diagnosis = diagnoses.Where(d => d.Id == prescription.DiagnosisId).FirstOrDefault();
+            }
+            return prescriptions;
+        }
+
         private IEnumerable<Diagnosis> FetchDiagnoses(IEnumerable<int> diagnosisIds) 
             => _diagnosisConnection.Post<IEnumerable<Diagnosis>>(diagnosisIds);
 
         public IEnumerable<MedicationPrescription> GetAll()
-            => _medicationPrescriptionWrapper.Repository.GetAll();
+            => AttachDiagnoses(_medicationPrescriptionWrapper.Repository.GetAll());
     }
 }
