9fe53c3df7ccf85842e3e21337d574f96d347ad5
Dimitrije Bulaja <dbulaja98@gmail.com>
2021-01-15 02:18:04 +0100

    Fix: TimeInterval problem fixed

--- WPFHospitalEditor/Model/TimeInterval.cs
+++ WPFHospitalEditor/Model/TimeInterval.cs
@@ -4,15 +4,13 @@ namespace WPFHospitalEditor.Model
 {
     public class TimeInterval
     {
-        public TimeInterval(DateTime startDate, DateTime endDate)
-        {
-            StartDate = startDate;
-            EndDate = endDate;
-        }
-
-        public DateTime StartDate { get; }
-        public DateTime EndDate { get; }
         public DateTime Start { get; internal set; }
         public DateTime End { get; internal set; }
+
+        public TimeInterval(DateTime startDate, DateTime endDate)
+        {
+            Start = startDate;
+            End = endDate;
+        }
     }
 }
