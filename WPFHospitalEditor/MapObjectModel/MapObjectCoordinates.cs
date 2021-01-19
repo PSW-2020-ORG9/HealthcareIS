89e8436d7a02fdd39f5d4188f40097de56326d56
Dimitrije Bulaja <dbulaja98@gmail.com>
2021-01-19 03:31:56 +0100

    MapObjectCoordinates is now ValueObject

--- WPFHospitalEditor/MapObjectModel/MapObjectCoordinates.cs
+++ WPFHospitalEditor/MapObjectModel/MapObjectCoordinates.cs
@@ -1,14 +1,26 @@
-﻿namespace WPFHospitalEditor.MapObjectModel
+﻿
+using WPFHospitalEditor.Exceptions;
+
+namespace WPFHospitalEditor.MapObjectModel
 {
     public class MapObjectCoordinates
     {
-        public double X { get; set; }
-        public double Y { get; set; }
+        public double X { get; }
+        public double Y { get; }
 
         public MapObjectCoordinates(double X, double Y)
         {
-            this.X = X;
-            this.Y = Y;
+            if(Validate(X, Y))
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+        }
+
+        public bool Validate(double x, double y)
+        {
+            if (x < 0 || y < 0) throw new ValidationException("X and Y must be greater then zero");
+            return true;
         }
     }
 }
