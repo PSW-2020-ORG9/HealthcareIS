423b393faf1e78d99f69c9e7f94610f811ce6fbb
Dimitrije Bulaja <dbulaja98@gmail.com>
2021-01-19 21:13:46 +0100

    Created tests for value objects

---
+++ WPFHospitalEditorUnitTests/ValueObjectTests/MapObjectCoordinatesTest.cs
@@ -0,0 +1,43 @@
+﻿using Shouldly;
+using WPFHospitalEditor.MapObjectModel;
+using Xunit;
+
+namespace WPFHospitalEditorUnitTests.ValueObjectTests
+{
+    public class MapObjectCoordinatesTest
+    {
+        [StaFact]
+        public void Create_valid_object()
+        {
+            double x = 5;
+            double y = 4;
+            MapObjectCoordinates mapObjectCoordinates = null;
+            try
+            {
+                mapObjectCoordinates = new MapObjectCoordinates(x, y);
+            }
+            catch
+            {
+
+            }
+            mapObjectCoordinates.ShouldNotBeNull();
+        }
+
+        [StaFact]
+        public void Create_invalid_object()
+        {
+            double x = -5;
+            double y = 4;
+            MapObjectCoordinates mapObjectCoordinates = null;
+            try
+            {
+                mapObjectCoordinates = new MapObjectCoordinates(x, y);
+            }
+            catch
+            {
+
+            }
+            mapObjectCoordinates.ShouldBeNull();
+        }
+    }
+}
---
+++ WPFHospitalEditorUnitTests/ValueObjectTests/MapObjectDimensionsTest.cs
@@ -0,0 +1,45 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WPFHospitalEditor.MapObjectModel;
+using Xunit;
+
+namespace WPFHospitalEditorUnitTests.ValueObjectTests
+{
+    public class MapObjectDimensionsTest
+    {
+        [StaFact]
+        public void Create_valid_object()
+        {
+            double height = 50;
+            double width = 40;
+            MapObjectDimensions mapObjectDimensions = null;
+            try
+            {
+                mapObjectDimensions = new MapObjectDimensions(height, width);
+            }
+            catch
+            {
+
+            }
+            mapObjectDimensions.ShouldNotBeNull();
+        }
+
+        public void Create_invalid_object()
+        {
+            double height = 50;
+            double width = -40;
+            MapObjectDimensions mapObjectDimensions = null;
+            try
+            {
+                mapObjectDimensions = new MapObjectDimensions(height, width);
+            }
+            catch
+            {
+
+            }
+            mapObjectDimensions.ShouldBeNull();
+        }
+    }
+}
