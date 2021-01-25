b81b51740155a77a8f2245127e5c03705f4d85d5
sStrbacki <57362581+sStrbacki@users.noreply.github.com>
2021-01-25 02:06:31 +0100

    Refactor: Leftover debugging code removed

--- HospitalFrontend/src/services/eventPublisher.js
+++ HospitalFrontend/src/services/eventPublisher.js
@@ -2,7 +2,6 @@ import api from '../constant/api.js'
 import axios from 'axios'
 
 export function publishSchedulingEvent(data){
-    console.log(data)
     axios.post(api.schedulingEventUrl,data).then(response=>{
         return response
     })
