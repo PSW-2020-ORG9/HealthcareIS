49c78f685d25ab4411765055765d13db09eb23ba
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2021-01-25 20:41:33 +0100

    added wpf action events repositories

---
+++ EventStore.API/Infrastructure/Repositories/WPFActionEventsRepositories/IFloorChangeActionEventRepository.cs
@@ -0,0 +1,13 @@
+﻿using EventStore.API.Model.EventStore.WPFActionEvents;
+using General.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventStore.API.Infrastructure.Repositories.WPFActionEventsRepositories
+{
+    interface IFloorChangeActionEventRepository : IWrappableRepository<FloorChangeActionEvent, int>
+    {
+    }
+}
